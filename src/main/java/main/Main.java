f4fcc7a75b0a7be5ab8d75ecef34ec148c85a5d6
Adminsvu <Baikonur96@yandex.ru>
2023-03-01 16:29:12 +0300

    ver.2.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
+        System.setProperty("server.port", "8081");
         SpringApplication.run(Main.class, args);
     }
 }
